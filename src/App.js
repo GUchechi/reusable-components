c36d5cc4cf1b4c94cb1c018e2bcefcbe02604988
GUchechi <godswilluchendu7@gmail.com>
2022-08-15 14:01:25 +0100

    used prop to pass data into input & created card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+
+import { useState } from 'react';
 import './App.css';
 import Input from './Components/Input';
 
 function App() {
+  const [firstInput, setFirstInput] = useState("")
+  const [secondInput, setSecondInput] = useState("")
+  const [thirdInput, setThirdInput] = useState("")
+  const [fourthInput, setFourthInput] = useState("")
   return (
     <div className="App">
-      <Input />
+     <Input
+      inputType="text"
+      inputValue={firstInput}
+      setInputValue={setFirstInput}
+      />
+     <Input
+      inputType="number"
+      inputValue={secondInput}
+      setInputValue={setSecondInput}
+      />
+     <Input
+      inputType="password"
+      inputValue={thirdInput}
+      setInputValue={setThirdInput}
+      />
+     <Input
+      inputType="range"
+      inputValue={fourthInput}
+      setInputValue={setFourthInput}
+      />
+
+     <div>
+      <p>First input:{firstInput}</p>
+      <p>Second input:{secondInput}</p>
+      <p>Third input:{thirdInput}</p>
+      <p>Fourth input:{fourthInput}</p>
+     </div>
     </div>
   );
 }
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Input = () => {
+const Input = ({inputType,inputValue, setInputValue}) => {
   return (
     <div>
-        <input text="Input" />
+        <input 
+          type={inputType}
+          value={inputValue}
+          onChange={ e => setInputValue(e.target.value)}
+        />
     </div>
   )
 }
